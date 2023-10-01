1724bf8f057c48e45eb017b3fb7994fa66f4ed60
Harun <haruntoker@live.com>
2023-10-01 13:55:05 +0300

    files created

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+require('express-async-errors');
+
+
+
+
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.static('./public'));
+app.use(bodyParser.json())
+app.use(express.json());
+
+//route middleware
+
+
+
+
+
+//before port & db
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+
+//env process - 1
+dotenv.config()
+const PORT = process.env.PORT || 4000
+const MONGO_URI = process.env.MONGO_URI
+
+//port & mongoDB process - 2
+const connect = async()=>{
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+  });
+
+      console.log("DB connected succesfully!");
+      app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+  })
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+connect()
+
+
